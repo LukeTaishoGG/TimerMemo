d50692c1123eb88269576397589b4fa0407dd568
Taisho <luke.wood.et@gmail.com>
2026-03-07 18:55:35 +0900

    Lint Pretter

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ function App() {
   const testMemo = addGetMemo
   const deleate = clearMemo
 
-
   const [memo,setMemo] = useState('')
 
   const [note, setNote] = useState(() => getNote() ?? "")
---
+++ src/type.ts
@@ -0,0 +1,8 @@
+export type memo = {
+  id: number
+  title: string
+  note: string
+  updateDate: Date
+  scheduledDeletionAt: Date
+  deleateAt?: Date
+}
